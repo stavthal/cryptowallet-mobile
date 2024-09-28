d1fcc370be7afd22f4b745e0dbbe0d6eef11ae42
Stavros Thalassinos <stavros8003@gmail.com>
2024-09-28 05:18:59 +0300

    setup

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+
+// Screens
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+function App(): React.JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
